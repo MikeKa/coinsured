aa3ac338f38e4d35a95a4b0d58905dd73379ce8c
MikeKa <mkashirsky@gmail.com>
2017-11-20 02:13:07 +0300

    mongo uri

--- config/production.js
+++ config/production.js
@@ -1,5 +1,5 @@
 module.exports = {
   // mongo url
-  mongoUrl: process.env.MONGODB_URI,
+  mongoUrl: "mongodb://heroku_hb6r821b:ogser4sa8qv78n2vfk6pn2ne6g@ds113906.mlab.com:13906/heroku_hb6r821b",
   fbCallback: "https://coinsured.herokuapp.com/auth/facebook/callback"
 };
